585019b49958412105fa4c9503c3df4b82067587
HeTrack <iliya.utd2014@gmail.com>
2019-12-21 02:15:04 +0400

    отмена фиксации

--- WindowsFormsShip/WindowsFormsShip/ParkingNotFoundException.cs
+++ WindowsFormsShip/WindowsFormsShip/ParkingNotFoundException.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsShip
 {
-    // или ObjectDisposedException - выбрасывается при попытке вызвать метод объекта, который уже был уничтожен (disposed)
-    //ArgumentNullException — производный от ArgumentException класс, выбрасывается если один из аргументов функции неожиданно равен null.
-    public class ParkingNotFoundException: ArgumentNullException
+    public class ParkingNotFoundException:Exception
     {
         public ParkingNotFoundException(int i):base("Не найден автомобиль по месту " + i)
         { }
