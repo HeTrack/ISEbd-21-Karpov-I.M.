8da34a13faadbb80f0fd7d94145804acca3ccaf7
HeTrack <iliya.utd2014@gmail.com>
2019-12-21 02:05:36 +0400

    специализированные классы ошибок

--- WindowsFormsShip/WindowsFormsShip/ParkingNotFoundException.cs
+++ WindowsFormsShip/WindowsFormsShip/ParkingNotFoundException.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsShip
 {
-    // или ObjectDisposedException
+    // или ObjectDisposedException - выбрасывается при попытке вызвать метод объекта, который уже был уничтожен (disposed)
+    //ArgumentNullException — производный от ArgumentException класс, выбрасывается если один из аргументов функции неожиданно равен null.
     public class ParkingNotFoundException: ArgumentNullException
     {
         public ParkingNotFoundException(int i):base("Не найден автомобиль по месту " + i)
--- WindowsFormsShip/WindowsFormsShip/ParkingOverflowException.cs
+++ WindowsFormsShip/WindowsFormsShip/ParkingOverflowException.cs
@@ -5,8 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsFormsShip
-{
-   public class ParkingOverflowException: Exception
+{// производный от ArgumentException класс, выбрасывается когда аргумент функции имеет слишком большое или слишком маленькое значение для данного типа
+    // или  IndexOutOfRangeException - Выбрасывается во время запуска при доступе в элемент массива с неправильным индексом.
+    public class ParkingOverflowException: ArgumentOutOfRangeException
     {
         public ParkingOverflowException(): base("На парковке нет свободных мест")
         { }
